3a37101de3ea9f83378e3cc6fd23d74767f7e77c
shogo.tsuji <tjsg06@star.cims.jp>
2021-07-13 06:53:26 +0000

    fizzbuzz

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,24 @@
 num_max = 100
-def fizzbuzz(n)
-if n % 15 == 0
-puts "FizzBuzz"
-elsif n % 3 == 0
-puts "Fizz"
-elsif n % 5 == 0
-puts "Buzz"
-else
-puts n
+
+def fizzbuzz(num)
+    if n % 15 == 0
+        return "FizzBuzz"
+    elsif n % 3 == 0
+        return "Fizz"
+    elsif n % 5 == 0
+        return "Buzz"
+    else num
+    end
 end
-end
-(1..num_max).each do |n|
-fizzbuzz(n)
+ 
+
+(1..num_max).each do |num|
+    if n % 15 == 0
+        puts "FizzBuzz"
+    elsif n % 3 == 0
+        puts "Fizz"
+    elsif n % 5 == 0
+        puts "Buzz"
+    else num
+    end
 end
