d658dac6b7740fd0dbb9c8607d3e7571a19763ca
shogo.tsuji <tjsg06@star.cims.jp>
2021-07-13 08:37:16 +0000

    fizzbuzz

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -14,13 +14,5 @@ end
  
 
 (1..num_max).each do |num|
-    if num % 3 == 0 && num % 5 ==0
-        puts "FizzBuzz"
-    elsif num % 3 == 0
-        puts "Fizz"
-    elsif num % 5 == 0
-        puts "Buzz"
-    else 
-        puts num
-    end
+    puts fizzbuzz(num)
 end
