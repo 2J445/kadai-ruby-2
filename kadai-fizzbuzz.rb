dfbde11abee5d5ab2f7fd8d1c959f81ef46049ba
shogo.tsuji <tjsg06@star.cims.jp>
2021-07-13 08:26:32 +0000

    fizzbuzz

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,24 +1,26 @@
 num_max = 100
 
 def fizzbuzz(num)
-    if n % 15 == 0
+    if num % 3 == 0 && num % 5 ==0
         return "FizzBuzz"
-    elsif n % 3 == 0
+    elsif num % 3 == 0
         return "Fizz"
-    elsif n % 5 == 0
+    elsif num % 5 == 0
         return "Buzz"
-    else num
+    else 
+        return num
     end
 end
  
 
-(1..num_max).each do |n|
-    if n % 15 == 0
+(1..num_max).each do |num|
+    if num % 3 == 0 && num % 5 ==0
         puts "FizzBuzz"
-    elsif n % 3 == 0
+    elsif num % 3 == 0
         puts "Fizz"
-    elsif n % 5 == 0
+    elsif num % 5 == 0
         puts "Buzz"
-    else n
+    else 
+        puts num
     end
 end
